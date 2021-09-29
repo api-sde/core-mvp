cc2ec8bbf2f5fff5473bb6d1d06582062b9325a5
api-sde <pietka.adrien@gmail.com>
2021-09-29 08:43:10 -0700

    Init project structure

---
+++ AgentMvp/Mvp.Core.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mvp.Core.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ AgentMvp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AgentMvp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            await GetWeather();
+            Console.ReadKey();
+        }
+        
+        
+        
+        private static async Task<string> GetWeather()
+        {
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync("http://localhost:5001/weatherforecast");
+            return result;
+        }
+    }
+}
